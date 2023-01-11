214a23f79728f4f6350b1b05ac97edb7747241ef
Weronika <weronika.piotrowska@tdcm.io>
2023-01-11 20:16:54 +0100

    add create method for user controller add method to set up encrypted key for create new user in user model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::UsersController < ApplicationController
+
+  def create
+    @user = User.new
+    if @user.save
+     render json: { api_key: @user.encrypted_key}, status: :ok
+    else
+     render json: { message: 'Unable to create User.' }, status: :bad_request
+    end 
+  end
 end
