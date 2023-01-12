b71fdcaf174b91d94bb8c59b6ece7cf22c484987
Weronika <weronika.piotrowska@tdcm.io>
2023-01-13 00:58:01 +0100

    fix rubocop offenses

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -4,20 +4,20 @@ class Api::V1::LinksController < ApplicationController
   def show
     @link = Link.find_by(id: params[:id])
     if @link&.belongs_to?(@user)
-      render json: { link: @link.serializable_hash.except("user_id") }, status: :ok
+      render json: { link: @link.serializable_hash.except('user_id') }, status: :ok
     elsif @link.present?
       render json: { message: 'You are unauthorized to perform this action.' }, status: :unauthorized
-    else 
+    else
       render json: { message: 'Unable to find Link.' }, status: :not_found
     end
   end
 
   def index
     if @user.links.any?
-      render json: { links: @user.links.map { |link| link.serializable_hash.except("user_id")} }, status: :ok
-     else
+      render json: { links: @user.links.map { |link| link.serializable_hash.except('user_id') } }, status: :ok
+    else
       render json: { message: 'Unable to find any Links.'}, status: :not_found
-     end
+    end
   end
 
   def create
@@ -29,7 +29,6 @@ class Api::V1::LinksController < ApplicationController
       render json: { message: @link.errors.full_messages }, status: :unprocessable_entity
     end
   end
-
 
   private
   def link_params
