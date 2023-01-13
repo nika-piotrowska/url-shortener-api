1d792b86e856cbd6f30b28b4f6e203ff54764da0
Weronika <weronika.piotrowska@tdcm.io>
2023-01-13 01:31:43 +0100

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      resources :links, only: [:create, :show, :index]
+      resources :links, only: [:create, :show, :index, :destroy]
     end
   end
   get '/', to: 'application#show_link'
