8abb4515d5044d52b15e4ec1d1b40a3fd4ece976
Weronika <weronika.piotrowska@tdcm.io>
2023-01-13 02:13:26 +0100

    add refresh_encrypted_key endpont for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,9 +4,18 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new
     if @user.save
-      render json: { api_key: @user.encrypted_key}, status: :ok
+      render json: { encrypted_key: @user.encrypted_key}, status: :ok
     else
       render json: { message: 'Unable to create User.' }, status: :bad_request
+    end
+  end
+
+  def refresh_encrypted_key
+    @user.encrypted_key = SecureRandom.uuid
+    if @user.save
+      render json: { encrypted_key: @user.encrypted_key, message: 'This is your new key.'  }, status: :ok
+    else
+      render json: { message: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 end
