2c4a80c1b9bd233a3362184820211d391c7c2f0d
Weronika <weronika.piotrowska@tdcm.io>
2023-01-13 02:13:32 +0100

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
+      resources :users, only: [:create] do
+        collection do
+          patch 'refresh_encrypted_key'
+        end
+      end
       resources :links, only: [:create, :show, :index, :destroy]
     end
   end
